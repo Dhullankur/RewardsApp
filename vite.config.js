b33fc66326a5050bc8eeff2cc5e0132b77367491
dhullankur <ankur007dhull@gmail.com>
2026-06-03 16:27:36 +0530

    Added github deployment script

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react({ jsxRuntime: 'automatic' }), tailwindcss()],
+  base: '/RewardsApp/',
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
     include: ['src/test/**/*.test.{js,jsx}'],
   },
-})
+});
